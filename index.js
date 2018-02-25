8282cfbae79030bf5ca36922e821dabfe8f21b86
Kaushal Patel <kaushalpatel778@gmail.com>
2018-02-25 00:24:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 function happyHolidaysTo(personName) {
-  return (`Happy holidays , ${personName}!`);
+  return (`Happy holidays ,  ${personName}!`);
 }
 function happyHolidayTo(holidayName,personName) {
   return (`Happy ${holidayName} , ${personName}!`);
