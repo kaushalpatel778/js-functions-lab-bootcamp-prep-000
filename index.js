f1cc2e5b5bd29f22a1b6a63a3e87e33a6fd6dc80
Kaushal Patel <kaushalpatel778@gmail.com>
2018-02-25 00:26:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function happyHolidays() {
   return "Happy holidays!";
 }
 function happyHolidaysTo(name) {
-  return (`Happy holidays ,  ${name}!`);
+  return (`Happy holidays, ${name}!`);
 }
 function happyHolidayTo(holiday,name) {
-  return (`Happy ${holiday} , ${name}!`);
+  return (`Happy ${holiday}, ${name}!`);
 }
 function holidayCountdown(holidayName,days){
   return (`It's ${days} days untill ${holiday}!`);
